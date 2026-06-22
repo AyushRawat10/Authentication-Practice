b3e512fbcd687923555774b0b3681489ccad487f
Ayush Rawat <rawatayushh10@gmail.com>
2026-06-22 13:51:18 +0530

    Hooks and Methods are done in userSchema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema({
   email: {
@@ -57,5 +59,38 @@ const userSchema = new Schema({
     timestamps: true
 }
 );
+
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateRefreshToken = async function () {
+    jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+    )
+}
+
+userSchema.methods.generateAccessToken = async function () {
+    jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            userName: this.userName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+    )
+}
 
 export const User = new mongoose.model("User", userSchema);
