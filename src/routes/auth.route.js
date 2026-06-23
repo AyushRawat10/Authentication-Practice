8c1136fec3d85f59c8546417c828b720a90dbba2
Ayush Rawat <rawatayushh10@gmail.com>
2026-06-23 21:40:44 +0530

    get-current user controller done

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import {
+  getCurrentUser,
   loginUser,
   logoutUser,
   registerUser,
@@ -11,5 +12,6 @@ const router = Router();
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
 
 export default router;
